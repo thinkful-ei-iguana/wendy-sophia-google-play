fd9620e09bedfd9a2066c49a0b6c590317cf2bdb
Wendy Bartos <wendyebartos@gmail.com>
2019-12-03 19:48:13 -0800

    added case insentive filter and sort

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
-const express = require('express');
-const morgan = require('morgan');
-const playstore = require('./playstore.js');
+/* eslint-disable quotes */
+const express = require("express");
+const morgan = require("morgan");
+const playstore = require("./playstore.js");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/apps', (req, res) => {
-    const { sort, genres } = req.query;
-    let results = playstore
+app.get("/apps", (req, res) => {
+  const { sort, genres } = req.query;
 
-    if (sort) {
-        if (!['app', 'rating'].includes(sort)) {
-            return res.status(400).send('Sort must be one of app or rating');
-        }
+  if (sort) {
+    if (!["app", "rating"].includes(sort)) {
+      return res.status(400).send("Sort must be one of app or rating");
     }
+  }
 
-    if (genres) {
-        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
-            return res.status(400).send('Genre must be included');
-        }
+  if (genres) {
+    if (
+      ["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(
+        genres
+      )
+    ) {
+      return res.status(400).send("Genre not found. Select another genre");
     }
+  }
+  let results = playstore.filter(app =>
+    app.Genres.toLowerCase().includes(genres)
+  );
 
-    if (genres) {
-         results = playstore
-            .filter(game => {
-                return game.Genres === genres
-            })
-    }
+  if (sort === "rating") {
+    results = playstore.sort((a, b) => (a.Rating < b.Rating ? -1 : 1));
+  }
+  if (sort === "app") {
+    results = playstore.sort((a, b) =>
+      a.App.toLowerCase() < b.App.toLowerCase() ? -1 : 1
+    );
+  }
 
-    if (sort) {
-
-        if (sort === 'rating') {
-            results = playstore.sort((a, b) => a.Rating < b.Rating ? -1 : 1);
-        }
-
-        if (sort === 'app') {
-            results = playstore.App.sort((a, b) => {
-                let res;
-                const aApp = a.App.toUppercase();
-                const bApp = b.App.toUppercase();
-
-                if(a.App > b.App){
-                    res = 1
-                }
-
-                else if(a.App < b.App){
-                    res = -1
-                }
-
-                return res
-            });
-        }
-    }
-
-    res.json(results);
-})
+  res.json(results);
+});
 
 app.listen(8080, () => {
-    console.log('Server started on PORT 8080')
-})
-
-
-
+  console.log("Server started on PORT 8080");
+});
